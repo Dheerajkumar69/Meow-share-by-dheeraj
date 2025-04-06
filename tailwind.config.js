0e4dea94c27847940e53680a45d261e0c5384a6b
Dheeraj kumar <dheerajsingh9933@gmail.com>
2025-04-06 22:39:07 +0530

    still trash

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+      },
       colors: {
         primary: {
           50: 'var(--primary-50)',
